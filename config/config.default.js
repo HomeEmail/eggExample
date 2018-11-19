418c24225c341b06dc77722d9c412137eb399232
HomeEmail <591694604@qq.com>
2018-11-19 20:28:50 +0800

    32

--- config/config.default.js
+++ config/config.default.js
@@ -15,14 +15,26 @@ exports.news = {
 	serverUrl:'https://hacker-news.firebaseio.com/v0'
 };
 
-//添加中间件 robot
+//在配置众中引入中间件,添加如下中间件，数组顺序即为中间件的加载顺序
 exports.middleware = [
-	'robot'
+	'robot',
+	'gzip'
 ];
+//框架和插件中使用中间件样例
+//app.config.coreMiddleware.unshift('gzip');
+
+//router 中使用中间件样例，只想针对单个路由生效，可以直接在 app/router.js 中实例化和挂载
+// const gzip = app.middleware.gzip({ threshold: 1024 });
+// app.router.get('/needgzip', gzip, app.controller.handler);
+
 
 //robot's configurations 禁止哪些爬虫访问
 exports.robot = {
 	ua:[
 		/Baiduspider/i
 	]
+};
+//gzip 中间件配置
+exports.gzip = {
+	threshold:1024, // 小于 1k 的响应体不压缩
 };
---
+++ readme.rd
@@ -0,0 +1,13 @@
+readme.rd
+
+调试开发
+npm run debug
+
+开发运行
+npm run dev
+
+测试
+npm run test
+
+测试覆盖测试
+npm run cov
